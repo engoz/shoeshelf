443fe8fd173198255f4975cf7d508c8c252062c2
engoz <engoz@msn.com>
2023-09-28 19:35:50 +0300

    add all file

--- src/main/java/com/shoeshelf/ShoeShelfApplication.java
+++ src/main/java/com/shoeshelf/ShoeShelfApplication.java
@@ -1,9 +1,7 @@
 package com.shoeshelf;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@EnableAutoConfiguration
 
 @SpringBootApplication
 public class ShoeShelfApplication {
